8091dbf3285ace7e35bb5ba2d42550a070cffbe3
karmakarsubhajit <subhajitkarmakar22@gmail.com>
2024-08-20 10:51:13 +0530

    Initial Project commit

---
+++ src/main/java/com/subhajit/city_temp_tracker/CityTempTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.subhajit.city_temp_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityTempTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityTempTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subhajit/city_temp_tracker/CityTempTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhajit.city_temp_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityTempTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
